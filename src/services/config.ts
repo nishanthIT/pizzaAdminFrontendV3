2d2e75d37553ee3d53c384ea506b2d51fb26d282
nishanth <nishanth23082004@gmail.com>
2025-08-05 16:28:44 +0530

    fixed config

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,4 +1,4 @@
 export const API_URL = `${import.meta.env.VITE_API_URL}/admin`;
-export const API_IMG_URL = `${import.meta.env.VITE_API_URL}/`;
+export const API_IMG_URL = `${import.meta.env.VITE_API_URL}`;
 // export const API_URL = "http://localhost:3003/api/admin";
 // export const API_IMG_URL = "http://localhost:3003/api";
